cef3edbd7e111f30b46b05912a73285c4289a9fb
Zack <zalsmith1996@gmail.com>
2024-01-18 21:40:25 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Elements
+const clear = document.querySelector('c');
+const equal = document.querySelector('=');
+const plus = document.querySelector('+');
+const minus = document.querySelector('-');
+const multiply = document.querySelector('*');
+const divide = document.querySelector('/');
+const zero = document.querySelector('0');
+const one = document.querySelector('1');
+const two= document.querySelector('2');
+const three = document.querySelector('3');
+const four = document.querySelector('4');
+const five = document.querySelector('5');
+const six = document.querySelector('6');
+const seven = document.querySelector('7');
+const eight = document.querySelector('8');
+const nine = document.querySelector('9');
+let display = document.querySelector('display');
+
+// User Input
+let firstNum = 0;
+let secondNum = 0;
+let solution = 0;
+let operator = null;
+
