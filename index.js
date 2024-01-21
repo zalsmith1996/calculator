4619c4f886307355cccfaabf6d16adbdb2c5b0f2
Zack <zalsmith1996@gmail.com>
2024-01-20 23:35:36 -0500

    Update functions and event listeners

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
-// Elements
-const clear = document.querySelector('c');
-const equal = document.querySelector('=');
-const plus = document.querySelector('+');
-const minus = document.querySelector('-');
-const multiply = document.querySelector('*');
-const divide = document.querySelector('/');
-const zero = document.querySelector('0');
-const one = document.querySelector('1');
-const two= document.querySelector('2');
-const three = document.querySelector('3');
-const four = document.querySelector('4');
-const five = document.querySelector('5');
-const six = document.querySelector('6');
-const seven = document.querySelector('7');
-const eight = document.querySelector('8');
-const nine = document.querySelector('9');
-let display = document.querySelector('display');
+// Elements 
+let display = document.querySelector('.display');
+let nodeList = document.querySelectorAll('button');
+const buttons = [...nodeList];
 
-// User Input
-let firstNum = 0;
-let secondNum = 0;
-let solution = 0;
-let operator = null;
+// If classlist includes 'operator' => operator
 
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.id) {
+            case ('number'):
+                display.value = e.target.value;
+                console.log('Number clicked.');
+            break;
+            case ('clear'):
+                console.log('Clear clicked.');
+            break;
+            case ('equal'):
+                console.log('Equal clicked.');
+            break;
+            case ('operator'):
+                console.log('Operator clicked.');
+            break;
+            default:
+                console.log('something');
+            break;
+        }
+    });
+});
+
+function storeNumbers(number) {
+    if (firstNum === '' || secondNum !== '') {
+        firstNum = number;
+        return firstNum;
+    } else {
+        secondNum = number;
+    }
+    return secondNum;
+}
